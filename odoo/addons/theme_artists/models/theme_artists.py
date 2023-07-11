74c8aedca74f14b71d28d882bde9565dd70566ca
Bharat T <bharath.teja@ishafoundation.org>
2023-07-11 09:37:06 +0530

    removed odoo 13

--- odoo/addons/theme_artists/models/theme_artists.py
+++
@@ -1,9 +0,0 @@
-from odoo import models
-
-
-class ThemeArtists(models.AbstractModel):
-    _inherit = 'theme.utils'
-
-    def _theme_artists_post_copy(self, mod):
-        # Color preset
-        self.enable_view('theme_common.option_colors_03_variables')
--- odoo/addons/theme_real_estate/models/theme_real_estate.py
+++
@@ -1,9 +0,0 @@
-from odoo import models
-
-
-class ThemeRealEstate(models.AbstractModel):
-    _inherit = 'theme.utils'
-
-    def _theme_real_estate_post_copy(self, mod):
-        # Color preset
-        self.enable_view('theme_common.option_colors_05_variables')
