622f3ac87e54ca6b52009c82606486d819aad0ab
Bharat T <bharath.teja@ishafoundation.org>
2023-07-12 09:42:39 +0530

    added security

--- addons/isha_meditator/models/isha_meditator.py
+++ addons/isha_meditator/models/isha_meditator.py
@@ -6,6 +6,6 @@ class IshaMeditator(models.Model):
     name = fields.Char(string="Name", required=True)
     dob = fields.Date(string="DoB")
     gender = fields.Selection([('male', 'Male'),('female', 'Female')])
-    programs = fields.One2many('isha.program')
+    programs = fields.Many2many('isha.program')
     level = fields.Selection([('basic', 'Basic'),('intermediate', 'Intermediate'), ('advanced', 'Advanced')], string='Level')
     interesed_in_it = fields.Boolean("Intereseted in IT?")
