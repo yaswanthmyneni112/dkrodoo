89efba78e5765e4776e194be935f4f28597bdda5
Bharat T <bharath.teja@ishafoundation.org>
2023-07-12 09:47:18 +0530

    added icon

--- addons/isha_meditator/models/isha_meditator.py
+++ addons/isha_meditator/models/isha_meditator.py
@@ -5,7 +5,7 @@ class IshaMeditator(models.Model):
 
     name = fields.Char(string="Name", required=True)
     dob = fields.Date(string="DoB")
-    gender = fields.Selection([('male', 'Male'),('female', 'Female')])
-    programs = fields.Many2many('isha.program')
+    gender = fields.Selection([('male', 'Male'),('female', 'Female')], string="Gender")
+    programs = fields.Many2many(comodel_name='isha.program', string="Programs")
     level = fields.Selection([('basic', 'Basic'),('intermediate', 'Intermediate'), ('advanced', 'Advanced')], string='Level')
     interesed_in_it = fields.Boolean("Intereseted in IT?")
