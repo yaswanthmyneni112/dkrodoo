4ac6618f4c0e713dce10b554997d07b1fc2b294d
Bharat T <bharath.teja@ishafoundation.org>
2023-07-13 14:23:21 +0530

    seq

--- addons/isha_meditator/models/isha_volunteer.py
+++ addons/isha_meditator/models/isha_volunteer.py
@@ -10,12 +10,19 @@ from datetime import datetime
 class IshaVolunteer(models.Model):
     _name = 'isha.volunteer'
 
+    vol_num = fields.Char("Volunteer Number", readonly=True)
     meditator_id = fields.Many2one(comodel_name="isha.meditator")
     volunteer_type = fields.Selection(list(constants.VOLUNTEER_TYPE.items()), string='Volunteer Type')
     start_date = fields.Date(string="Start Date")
     end_date = fields.Date(string="End Date")
     visit_status = fields.Selection(list(constants.VOLUNTEER_VISIT_STATUS.items()), string='Volunteer Visit Status', default="yet_to_arrive")
 
+    @api.model
+    def create(self, vals):
+        if vals.get('vol_num', 'New') == 'New':
+            vals['vol_num'] = self.env['ir.sequence'].next_by_code('isha.volunteer.number') or 'Error: Sequence not found!'
+        return super(IshaVolunteer, self).create(vals)
+    
     @api.constrains("start_date", "end_date")
     def _check_visit_duration(self):
         for rec in self:
